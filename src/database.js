e463560adf1aab31d4d9ca0015ae444bdebcd30d
marmol891 <57042963+marmol891@users.noreply.github.com>
2019-10-26 19:04:00 +0200

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://marmol89:marmol89123@cluster0-mb4az.gcp.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('localhost', {
     useNewUrlParser: true
 })
   .then(db => console.log('db connected'))
